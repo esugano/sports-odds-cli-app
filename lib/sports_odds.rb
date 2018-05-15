c83deffd7eec635a1bbab22d706c3b3250fe567c
Emily S <emily.sugano@gmail.com>
2018-05-15 13:06:55 +0000

    Build out #make_booking_company and #list_booking_companies in odds.rb and add require link in sports_odds.rb

--- lib/sports_odds.rb
+++ lib/sports_odds.rb
@@ -6,6 +6,7 @@ require_relative "sports_odds/version"
 require_relative "sports_odds/scraper"
 require_relative "sports_odds/cli"
 require_relative "sports_odds/odds"
+require_relative "sports_odds/booking_companies"
 
 module SportsOdds
   # Your code goes here...
--- lib/sports_odds/odds.rb
+++ lib/sports_odds/odds.rb
@@ -1,6 +1,6 @@
 class SportsOdds::Odds
 
-  def make_booking_company
+  def self.make_booking_company
     SportsOdds::BookingCompanies.new("bookmaker","book-93")
     SportsOdds::BookingCompanies.new("5dimes","book-19")
     SportsOdds::BookingCompanies.new("heritage","book-169")
@@ -8,9 +8,16 @@ class SportsOdds::Odds
     SportsOdds::BookingCompanies.new("bovada","book-999996")
     SportsOdds::BookingCompanies.new("intertops","book-180")
     SportsOdds::BookingCompanies.new("youwager","book-139ß")
+    SportsOdds::BookingCompanies.all
   end
 
-  def choose_booking_company(company)
+  def self.list_booking_companies
+    self.make_booking_company.each do |booking_company|
+      puts booking_company.name.capitalize
+    end
+  end
+
+  def self.choose_booking_company(company)
     #get css class names based on company
     # list companies
     # if company == one of the companies
