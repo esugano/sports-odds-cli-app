b3af6efcaad4fb8bc1e16d334bc45bf72d676024
Emily S <emily.sugano@gmail.com>
2018-05-15 19:50:33 +0000

    Update #choose_booking_company with detect instead of each iteration

--- lib/sports_odds/odds.rb
+++ lib/sports_odds/odds.rb
@@ -1,5 +1,5 @@
 class SportsOdds::Odds
-  
+
   def self.make_booking_company
     SportsOdds::BookingCompanies.new("bookmaker","book-93")
     SportsOdds::BookingCompanies.new("5dimes","book-19")
@@ -10,7 +10,7 @@ class SportsOdds::Odds
     SportsOdds::BookingCompanies.new("youwager","book-139ß")
     SportsOdds::BookingCompanies.all
   end
-  
+
 #list booking companies
   def self.list_booking_companies
     self.make_booking_company.each do |booking_company|
@@ -20,10 +20,13 @@ class SportsOdds::Odds
 
 #get css class names (to use in scraping) based on booking company
   def self.choose_booking_company(company)
-    SportsOdds::BookingCompanies.all.each do |booking_company|
+    SportsOdds::BookingCompanies.all.detect do |booking_company|
       if booking_company.name == company.downcase
         booking_company.class_name
       end
     end
   end
 end
+
+# SportsOdds::Odds.choose_booking_company(company)
+# SportsOdds::Odds.list_booking_companies
